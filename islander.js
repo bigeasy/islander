170be64ffc1558f528472262353f3b0b2159fcbe
Alan Gutierrez <alan@prettyrobots.com>
2018-06-12 13:00:57 -0500

    Remap flush promises.
    Resolves a race condition where a flush is sent due to a collapse and
    then a subsequent government arrives with an empty map before the flush
    arrives. We were not aware that we'd flushed and attempted to remap our
    outstanding messages with the empty map.
    
    Remapping the flush gives us something to do instead of barreling on
    ahead with the outstanding message promise remap.
    
    No tests yet. I'm probably going to redo them all.

--- islander.js
+++ islander.js
@@ -1,4 +1,18 @@
-// TODO Optionally surrender retries on collapse.
+// Promise remapping is not required to function. You could post, track cookies
+// and flush on failure or if we get a government that is mapped as collapsed.
+// We'd trust that Paxos is remapping so long as the government doesn't
+// collapse.
+//
+// There is a race involving whether you got confirmation before or after
+// collapse that promises make unambiguous and at the time of writing I'm not
+// going to assume you're simply err on the side of flushing. It's probably
+// worth it to fuss with promises to resolve this ambiguity, but then remapping
+// is probably not necessary to function.
+//
+// Remapping is, however, a powerful assertion that these different different
+// algorithms are working together properly. We're asserting that Paxos is
+// remapping completely and correctly.
+//
 var assert = require('assert')
 var Monotonic = require('monotonic').asString
 var cadence = require('cadence')
@@ -76,14 +90,12 @@ Islander.prototype._nudge = function () {
 
 //
 Islander.prototype._flush = function () {
-    if (this._seeking.cookie == null) {
-        this._seeking.flushing = true
-        this._seeking.cookie = this._nextCookie()
-        this.outbox.push({
-            cookie: this._seeking.cookie,
-            messages: [{ id: this.id, cookie: this._seeking.cookie, body: null }]
-        })
-    }
+    this._seeking.cookie = this._nextCookie()
+    this._seeking.flushing = true
+    this.outbox.push({
+        cookie: this._seeking.cookie,
+        messages: [{ id: this.id, cookie: this._seeking.cookie, body: null }]
+    })
 }
 
 // TODO Need to timeout flushes, make sure we're not hammering a broken
@@ -98,9 +110,10 @@ Islander.prototype._flush = function () {
 Islander.prototype.sent = function (cookie, receipts) {
     if (this._seeking.cookie == cookie) {
         if (receipts == null) {
-            this._seeking.cookie = null
             this._flush()
-        } else if (!this._seeking.flushing) {
+        } else if (this._seeking.flushing) {
+            this._seeking.promise = receipts[this._seeking.cookie]
+        } else {
             this._seeking.messages.forEach(function (message) {
                 message.promise = receipts[message.cookie]
             })
@@ -143,22 +156,37 @@ Islander.prototype.push = function (entry) {
     if (Monotonic.isBoundary(entry.promise, 0)) {
         var map = entry.body.map
         if (map == null) {
-            // We will never remap anything so if any cookie we're using for
-            // flushing will now be invalid.
-            this._seeking.cookie = null
-        }
-        if (map == null || this._seeking.messages[0].promise == null) {
-            // Failed a remap because of a consensus collapse or else we didn't
-            // get our receipts in time.
+            // Government collapse so all pending messages have been discarded.
             this._flush()
-        } else if (Monotonic.compare(this._seeking.messages[0].promise, entry.promise) < 0) {
-            // Remap.
-            this._seeking.messages.forEach(function (message) {
-                message.promise = map[message.promise]
-            })
-            assert(this._seeking.messages.reduce(function (remapped, message) {
-                return remapped && message.promise != null
-            }, true), 'remap did not remap all posted entries')
+        } else if (this._seeking.flushing) {
+            if (this._seeking.promise == null) {
+                this._flush()
+            } else {
+                // This government entry may procede our request and response so
+                // that the promise we're waiting for in order to flush comes after
+                // the government and is therefore not remapped.
+                if (Monotonic.compare(this._seeking.promise, entry.promise) < 0) {
+                    this._seeking.promise = map[this._seeking.promise]
+                    assert(this._seeking.promise, 'remap did not remap all posted entries')
+                }
+            }
+        } else if (this._seeking.messages[0].promise != null) {
+            // This government entry may procede our request and response so
+            // that the first promise we're waiting for comes after the
+            // government and is therefore not remapped.
+            if (Monotonic.compare(this._seeking.messages[0].promise, entry.promise) < 0) {
+                // Remap.
+                this._seeking.messages.forEach(function (message) {
+                    message.promise = map[message.promise]
+                })
+                assert(this._seeking.messages.reduce(function (remapped, message) {
+                    return remapped && message.promise != null
+                }, true), 'remap did not remap all posted entries')
+            }
+        } else {
+            // We received a new government entry before we received promises we
+            // could receive promises used to remap.
+            this._flush()
         }
     // If this entry does pertains to us, look closer.
     } else if (this.id == entry.body.id) {
